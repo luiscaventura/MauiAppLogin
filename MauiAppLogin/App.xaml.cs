c98873931cae3d8cb8b4dc174ebcff77cf7cb7be
User <User@BP20230309>
2024-10-27 09:31:34 -0300

    Finalização do projeto login

--- MauiAppLogin/App.xaml.cs
+++ MauiAppLogin/App.xaml.cs
@@ -6,7 +6,29 @@
         {
             InitializeComponent();
 
-            MainPage = new AppShell();
+            string? usuario_logado = null;
+
+            // Enviando o usuário ara página de login por padrão.
+            MainPage = new Login();
+
+            Task.Run(async () =>
+            {
+                usuario_logado = await SecureStorage.Default.GetAsync("usuario_logado");
+
+                if (usuario_logado != null)
+                {
+                    MainPage = new Protegida();
+                }
+
+                if(usuario_logado == null)
+                {
+                    MainPage = new Login();
+
+                } else
+                {
+                    MainPage = new Protegida();
+                }
+            });
         }
 
         protected override Window CreateWindow(IActivationState activationState)
@@ -18,7 +40,6 @@
 
             return window;
         }
-    } //Fecha Classe
 
-    
-} //Fecha namespace
+    } // Fecha classe
+} // Fecha namespace
